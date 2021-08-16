9da66cafdfa277a61ec6a2f74fc68dc2949e9c48
sberberovic <sasa.berberovic@openanalytics.eu>
2021-08-16 08:21:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+To start the application execute following command:
+ 1. `mvn clean install`
+ 2. `docker-compose up`
---
+++ src/test/java/eu/openanalytics/phaedra/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.openanalytics.phaedra.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
