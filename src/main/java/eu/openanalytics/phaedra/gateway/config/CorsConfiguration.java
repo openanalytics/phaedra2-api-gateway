d7dd888e6b0bc3ec2bd1344219078cab260e7c37
sberberovic <sasa.berberovic@openanalytics.eu>
2021-12-14 17:28:42 +0100

    Add global CorsConfiguration

---
+++ src/main/java/eu/openanalytics/phaedra/gateway/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package eu.openanalytics.phaedra.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsConfiguration implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+    }
+}
