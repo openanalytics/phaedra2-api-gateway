67e87b0e58c0009beeb9a90ef2d5d9764c4b0fc0
Frederick Michielssen <fmichielssen@openanalytics.eu>
2021-12-14 15:20:54 +0100

    Trying a different way of configuring CORS

--- src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
+++ src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
@@ -2,11 +2,6 @@ package eu.openanalytics.phaedra.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.config.CorsRegistration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 /**
  * https://spring.io/blog/2019/08/16/securing-services-with-spring-cloud-gateway
@@ -15,21 +10,9 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
  */
 
 @SpringBootApplication
-@EnableWebFlux
 public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(GatewayApplication.class);
 		app.run(args);
-	}
-
-	@Bean
-	public WebFluxConfigurer corsConfigurer() {
-		return new WebFluxConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				CorsRegistration registration = registry.addMapping("/**");
-				registration.allowedMethods("*");
-			}
-		};
 	}
 }
