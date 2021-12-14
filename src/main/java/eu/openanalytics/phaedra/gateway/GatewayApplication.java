f6de0cb9c6e36b4b12f197c0f809245a7208974e
sberberovic <sasa.berberovic@openanalytics.eu>
2021-12-14 12:20:00 +0100

    Added CORS config: allow all methods and origins.

--- src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
+++ src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
@@ -2,17 +2,34 @@ package eu.openanalytics.phaedra.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 /**
  * https://spring.io/blog/2019/08/16/securing-services-with-spring-cloud-gateway
- * 
+ *
  * https://developer.okta.com/blog/2020/08/14/spring-gateway-patterns#pattern-1-openid-connect-authentication
  */
 
 @SpringBootApplication
+@EnableWebFlux
 public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(GatewayApplication.class);
 		app.run(args);
+	}
+
+	@Bean
+	public WebFluxConfigurer corsConfigurer() {
+		return new WebFluxConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				CorsRegistration registration = registry.addMapping("/**");
+				registration.allowedMethods("*");
+			}
+		};
 	}
 }
