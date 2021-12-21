f6e2678c08fb082ef4ab3a3e851ef8fa79b8eb67
Frederick Michielssen <fmichielssen@openanalytics.eu>
2021-12-21 15:09:16 +0100

    Added UI router, userinfo endpoint, oauth2 on the UI

--- src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
+++ src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package eu.openanalytics.phaedra.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
  * https://spring.io/blog/2019/08/16/securing-services-with-spring-cloud-gateway
@@ -11,8 +14,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
+
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(GatewayApplication.class);
 		app.run(args);
+	}
+
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		return http.authorizeExchange()
+				// API requests are routed freely (for now), the endpoint may choose to deny the request.
+				.pathMatchers("/api/**").permitAll()
+				// The remaining requests, i.e. UI requests, must follow the OAuth2 authorization flow
+				.anyExchange().authenticated()
+			.and().oauth2Login()
+			.and().build();
 	}
 }
---
+++ src/main/java/eu/openanalytics/phaedra/gateway/UserInfo.java
@@ -0,0 +1,22 @@
+package eu.openanalytics.phaedra.gateway;
+
+import java.security.Principal;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserInfo {
+
+	@RequestMapping(value = "/userinfo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getUserInfo(Principal principal) {
+		OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) principal;
+		OidcUser user = (OidcUser) token.getPrincipal();
+        return ResponseEntity.ok(user.getUserInfo());
+    }
+}
