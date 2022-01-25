810a6c7514e91bed42cd77bdba30392f7ca73267
Frederick Michielssen <fmichielssen@openanalytics.eu>
2022-01-25 15:19:51 +0100

    Enabled CORS on the userinfo endpoint

--- src/main/java/eu/openanalytics/phaedra/gateway/UserInfo.java
+++ src/main/java/eu/openanalytics/phaedra/gateway/UserInfo.java
@@ -6,10 +6,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class UserInfo {
 
