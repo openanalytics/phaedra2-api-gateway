d9ed94790c236a42f403f3dfcb295bf01d39b6a1
sberberovic <sasa.berberovic@openanalytics.eu>
2024-01-26 16:15:27 +0100

    fix: testing something

--- src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
+++ src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
@@ -56,17 +56,17 @@ public class GatewayApplication {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http.authorizeExchange()
-//                // API requests are routed freely (for now), the endpoint may choose to deny the request.
-////                .pathMatchers("/api/**").permitAll()
+                // API requests are routed freely (for now), the endpoint may choose to deny the request.
+                .pathMatchers("/api/**").permitAll()
 //                // The userinfo endpoint is accessible freely. Without an authenticated session, there is nothing to see.
 ////                .pathMatchers("/userinfo").permitAll()
 //                // The Swagger UI pages is accessible freely (for now)
-////                .pathMatchers("/*/swagger-ui.html").permitAll()
-////                .pathMatchers("/*/swagger-ui/**").permitAll()
-////                .pathMatchers("/v3/api-docs/**").permitAll()
+//                .pathMatchers("/*/swagger-ui.html").permitAll()
+//                .pathMatchers("/*/swagger-ui/**").permitAll()
+//                .pathMatchers("/v3/api-docs/**").permitAll()
 //                // GraphQL related endpoints are routed freely (for now)
-////                .pathMatchers("/graphiql").permitAll()
-////                .pathMatchers("/graphql").permitAll()
+                .pathMatchers("/graphiql").permitAll()
+                .pathMatchers("/graphql").permitAll()
 //                // The remaining requests, i.e. UI requests, must follow the OAuth2 authorization flow
                 .anyExchange().authenticated()
                 .and().logout().logoutSuccessHandler(phaedraLogoutHandler)
