6b966929b8080f3c609f52a22d930e078cbdb802
sberberovic <sasa.berberovic@openanalytics.eu>
2024-02-02 17:33:26 +0100

    test: implementation of LogoutController

--- src/main/java/eu/openanalytics/phaedra/gateway/controller/LogoutController.java
+++ src/main/java/eu/openanalytics/phaedra/gateway/controller/LogoutController.java
@@ -3,6 +3,8 @@ package eu.openanalytics.phaedra.gateway.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.server.DefaultServerRedirectStrategy;
+import org.springframework.security.web.server.ServerRedirectStrategy;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 import org.springframework.security.web.server.savedrequest.ServerRequestCache;
@@ -26,6 +28,7 @@ public class LogoutController {
 
     private final ServerSecurityContextRepository securityContextRepository = new WebSessionServerSecurityContextRepository();
     private final ServerRequestCache requestCache = new WebSessionServerRequestCache();
+    private final ServerRedirectStrategy redirectStrategy = new DefaultServerRedirectStrategy();
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -38,11 +41,11 @@ public class LogoutController {
                     return securityContextRepository.save(exchange, null);
                 })
                 .then(Mono.defer(() -> Mono.just(exchange.getResponse())))
-                .flatMap(response -> {
-                    String logoutUri = "https://keycloak.phaedra.poc.openanalytics.io/auth/realms/phaedra2/protocol/openid-connect/logout?redirect_uri=https://phaedra.poc.openanalytics.io/phaedra/ui";
-                    logger.info(String.format("logoutUri: %s", logoutUri));
-                    response.getHeaders().setLocation(URI.create(logoutUri));
-                    return response.setComplete();
-                });
+                .then(redirectStrategy.sendRedirect(exchange, createLogoutUri(exchange)));
+    }
+
+    private URI createLogoutUri(ServerWebExchange exchange) {
+        String logoutUri = "https://keycloak.phaedra.poc.openanalytics.io/auth/realms/phaedra2/protocol/openid-connect/logout?redirect_uri=https://phaedra.poc.openanalytics.io/phaedra/ui";
+        return URI.create(logoutUri);
     }
 }
