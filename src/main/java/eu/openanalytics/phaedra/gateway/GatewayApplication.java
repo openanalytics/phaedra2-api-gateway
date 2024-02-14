cc2cf836c03fd53d7a2ccfd21a29bbd6e03f448a
sberberovic <sasa.berberovic@openanalytics.eu>
2024-02-14 09:30:40 +0100

    update: add copyright headers and cleanup unused code

--- src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
+++ src/main/java/eu/openanalytics/phaedra/gateway/GatewayApplication.java
@@ -1,34 +1,27 @@
 /**
  * Phaedra II
- * <p>
+ *
  * Copyright (C) 2016-2024 Open Analytics
- * <p>
+ *
  * ===========================================================================
- * <p>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the Apache License as published by
  * The Apache Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * Apache License for more details.
- * <p>
+ *
  * You should have received a copy of the Apache License
  * along with this program.  If not, see <http://www.apache.org/licenses/>
  */
 package eu.openanalytics.phaedra.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.logout.*;
 
 /**
  * https://spring.io/blog/2019/08/16/securing-services-with-spring-cloud-gateway
